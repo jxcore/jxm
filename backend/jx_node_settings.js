9fb3e6f78bbac4ae89a976d0b233074a6c46244c
Oguz Bastemur <ob@zondig.com>
2014-05-13 12:33:04 +0200

    readme link is fixed

--- backend/jx_node_settings.js
+++ backend/jx_node_settings.js
@@ -46,7 +46,7 @@ exports.listenerTimeout = 60000; //long polling request time (ms) NOTE: max 120 
 exports.IPAddress = "0.0.0.0";
 
 exports.chunked = false;
-exports.mapiVersion = "0.2.6";
+exports.mapiVersion = "0.2.7";
 
 //enable disable console logging
 exports.console = true;
